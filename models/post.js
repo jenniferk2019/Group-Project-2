0d6a0b0d6f2889fef141135a5c2c013c4dd71602
Danit Lentz <danit@Danits-MacBook-Pro.local>
2019-08-13 13:03:01 -0400

    fixed mysql connection

---
+++ models/post.js
@@ -0,0 +1,9 @@
+module.exports = function(sequelize, Sequelize) {
+  var Hangout = sequelize.define("hangout", {
+    name: Sequelize.STRING,
+    location: Sequelize.STRING,
+    date: Sequelize.DATE,
+    user: Sequelize.STRING
+  });
+  return Hangout;
+}
